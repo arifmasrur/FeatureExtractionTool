28c7e1339d6a63e24396507f072ea09441c6b12c
arifmasrur <axm733@psu.edu>
2019-08-25 17:17:03 -0400

    Tested opencv functions for image read and display

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <string>
+#include <opencv2/opencv.hpp>
 
 using namespace std;
+using namespace cv;
 
 int main(int argc, char** argv) {
+	Mat img;
+	img = imread("baboon.png");
 
-	printf("Hello world! \n");
-	system("pause");
+	if (img.empty()) {
+		printf("Error is reading image!\n");
+		return 0;
+	}
+
+	string WindowName = "Display raw image";
+	namedWindow(WindowName, WINDOW_AUTOSIZE);
+	imshow(WindowName, img);
+
+	waitKey();
 
 	return 0;
+
+
 }
